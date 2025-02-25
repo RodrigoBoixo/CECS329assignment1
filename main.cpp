8f2dfc2fdb1c9e12105c3d5a84e3bc65e1d80b47
RodrigoBoixo <32583698+RodrigoBoixo@users.noreply.github.com>
2025-02-24 19:20:30 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int stateTransition(int currentState, char inputChar);
+
+int main()
+{
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);  
+
+   
+    if (input.empty()) {
+        cout << "reject" << endl;
+        return 0;
+    }
+
+    int currentState = 0; 
+
+    for (char c : input) {
+        currentState = stateTransition(currentState, c);
+    }
+
+    if (currentState == 11) {
+        cout << "accept" << endl;
+    } else {
+        cout << "reject" << endl;
+    }
+
+    return 0;
+}
+
+int stateTransition(int currentState, char inputChar)
+{
+    const string target = "gracehopper";
+
+    if (inputChar == target[currentState]) {
+        return currentState + 1;  
+    }
+
+    return currentState;
+}
+
